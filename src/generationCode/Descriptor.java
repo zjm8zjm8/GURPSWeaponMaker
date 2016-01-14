4b9d91a723e95296d6f4b744ba42a465328f84e9
Zach Myers <zach.myers@canyonsdistrict.org>
2016-01-13 17:12:07 -0700

    More on files, started file reading.

--- src/generationCode/Descriptor.java
+++ src/generationCode/Descriptor.java
@@ -1,5 +1,40 @@
 package generationCode;
 
 public class Descriptor {
-
+	private int position;
+	private int value;
+	private String name;
+	private String description;
+	private String effect;
+	
+	public Descriptor(int p, int v, String n, String d, String e)
+	{
+		position = p;
+		value = v;
+		name = n;
+		description = d;
+		effect = e;
+	}
+	
+	public int getPosition()
+	{
+		return position;
+	}
+	public int getValue()
+	{
+		return value;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public String getDescription()
+	{
+		return description;
+	}
+	public String getEffect()
+	{
+		return effect;
+	}
+	
 }
--- src/generationCode/Generator.java
+++ src/generationCode/Generator.java
@@ -1,9 +1,21 @@
 package generationCode;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Basically a wrapper for all of the rest of the code.  Generates items.
 public class Generator {
+	
+	public static ArrayList<Descriptor> base = new ArrayList<Descriptor>();
+	public static ArrayList<Descriptor> owner = new ArrayList<Descriptor>();
+	public static ArrayList<Descriptor> mod = new ArrayList<Descriptor>();
+	public static ArrayList<Descriptor> second = new ArrayList<Descriptor>();
+	public static ArrayList<Descriptor> lucky = new ArrayList<Descriptor>();
+	public static ArrayList<Descriptor> adv = new ArrayList<Descriptor>();
+	
 	public static void main(String[] args)
 	{
+		base = readList("./BaseItems", false);
 		int[] a = Calculations.doRandomDieRolls(100, 10, true);
 		int total = 0;
 		for (int i = 0; i < 20; i++)
@@ -16,5 +28,18 @@ public class Generator {
 			System.out.println();
 		}
 		System.out.println(total);
+	}
+	
+	public static ArrayList<Descriptor> readList(String filename, boolean ReadPosition)
+	{
+		Scanner s = new Scanner(filename);
+		Object[] stuff = new Object[4];
+		while (s.hasNext())
+		{
+			String curr = s.nextLine();
+		}
+		if (ReadPosition)
+		{}
+		return null;
 	}
 }
